679e5ccc0eab89a8390269a6c43719bf8609e67b
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2019-06-12 11:57:24 -0300

    1/2 frmEmprestimo

--- apBiblioteca/apBiblioteca/FrmEmprestimos.Designer.cs
+++ apBiblioteca/apBiblioteca/FrmEmprestimos.Designer.cs
@@ -28,19 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtCodLeitor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodLivro = new System.Windows.Forms.TextBox();
+            this.Emprestar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
+            // 
+            // txtCodLeitor
+            // 
+            this.txtCodLeitor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodLeitor.Location = new System.Drawing.Point(232, 57);
+            this.txtCodLeitor.Name = "txtCodLeitor";
+            this.txtCodLeitor.Size = new System.Drawing.Size(226, 26);
+            this.txtCodLeitor.TabIndex = 0;
+            this.txtCodLeitor.Leave += new System.EventHandler(this.txtCodLeitor_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Código Leitor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(96, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Código Livro:";
+            // 
+            // txtCodLivro
+            // 
+            this.txtCodLivro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodLivro.Location = new System.Drawing.Point(232, 114);
+            this.txtCodLivro.Name = "txtCodLivro";
+            this.txtCodLivro.Size = new System.Drawing.Size(226, 26);
+            this.txtCodLivro.TabIndex = 3;
+            this.txtCodLivro.Leave += new System.EventHandler(this.txtCodLivro_Leave);
+            // 
+            // Emprestar
+            // 
+            this.Emprestar.Location = new System.Drawing.Point(232, 232);
+            this.Emprestar.Name = "Emprestar";
+            this.Emprestar.Size = new System.Drawing.Size(140, 51);
+            this.Emprestar.TabIndex = 4;
+            this.Emprestar.Text = "Emprestar";
+            this.Emprestar.UseVisualStyleBackColor = true;
+            this.Emprestar.Click += new System.EventHandler(this.Emprestar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(70, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Data Devolução:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(232, 169);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(108, 26);
+            this.maskedTextBox1.TabIndex = 7;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
             // 
             // FrmEmprestimos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(674, 407);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Emprestar);
+            this.Controls.Add(this.txtCodLivro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodLeitor);
             this.Name = "FrmEmprestimos";
-            this.Text = "FrmEmprestimos";
+            this.Text = " Empréstimos";
+            this.Load += new System.EventHandler(this.FrmEmprestimos_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCodLeitor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodLivro;
+        private System.Windows.Forms.Button Emprestar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
     }
 }
--- apBiblioteca/apBiblioteca/FrmEmprestimos.cs
+++ apBiblioteca/apBiblioteca/FrmEmprestimos.cs
@@ -12,9 +12,71 @@ namespace apBiblioteca
 {
     public partial class FrmEmprestimos : Form
     {
+
+        Livro oLivro;
+        Leitor oLeitor;
+        VetorDados<Leitor> osLeitores;
+        VetorDados<Livro> osLivros;
         public FrmEmprestimos()
         {
             InitializeComponent();
+        }
+
+
+        private void FrmEmprestimos_Load(object sender, EventArgs e)
+        {
+            oLivro = new Livro();
+            oLeitor = new Leitor();
+            osLeitores = new VetorDados<Leitor>(50);
+            osLivros = new VetorDados<Livro>(50);
+        }
+
+        private void Emprestar_Click(object sender, EventArgs e)
+        {
+            oLeitor.QuantosLivrosComLeitor++;
+          //  oLeitor.CodigoLivroComLeitor[] == "";
+        }
+
+        private void txtCodLeitor_Leave(object sender, EventArgs e)
+        {
+            Leitor qualLeitor = new Leitor(txtCodLeitor.Text);
+            int onde = -1;
+            if (!osLeitores.Existe(qualLeitor, ref onde))
+            {
+                MessageBox.Show("O leitor não está cadastrado!");
+                LimparFocar(txtCodLeitor);
+            }
+            else
+                oLeitor = qualLeitor;
+        }
+
+        private void txtCodLivro_Leave(object sender, EventArgs e)
+        {
+            Livro qualLivro = new Livro(txtCodLivro.Text);
+            int onde = -1;
+            if (osLivros.Existe(qualLivro, ref onde))
+            {
+                if (qualLivro.CodigoLeitorComLivro == "000000")
+                {
+                    oLivro = qualLivro;
+                }
+                else
+                {
+                    MessageBox.Show("Livro já está emprestado!");
+                    LimparFocar(txtCodLivro);
+                }
+            }
+            else
+            {
+                MessageBox.Show("O livro não está cadastrado!");
+                LimparFocar(txtCodLivro);
+            }                
+        }
+
+        void LimparFocar(TextBox qualTxt)
+        {
+            qualTxt.Clear();
+            qualTxt.Focus();
         }
     }
 }
